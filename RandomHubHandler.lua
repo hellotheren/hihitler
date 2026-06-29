8bedb63258ba9bceecabb0962693a4d931971c44
hellotheren <kirilldk20@gmail.com>
2026-06-29 14:38:36 +0300

    Update RandomHubHandler.lua

--- RandomHubHandler.lua
+++ RandomHubHandler.lua
@@ -1,1 +1,10 @@
-
+local games = {
+    [18537175269]  = "https://pastebin.com/raw/8797sjkC"
+}
+repeat
+wait()
+until game.Players.LocalPlayer.Character
+if games[game.PlaceId] ~= nil then
+    print(games[game.PlaceId])
+    loadstring(game:HttpGet(games[game.PlaceId]))()
+end
