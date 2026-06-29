48df9cd269a71b0101d818343a2db2b90c86c8bd
hellotheren <kirilldk20@gmail.com>
2026-06-29 23:09:14 +0300

    Update d9rwes.lua

--- d9rwes.lua
+++ d9rwes.lua
@@ -1,4 +1,5 @@
 loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+--[[
 CMDs[#CMDs + 1] = {NAME = '', DESC = ''}
 CMDs[#CMDs + 1] = {NAME = 'randomhub / hub', DESC = 'Will load random hub'}
 CMDs[#CMDs + 1] = {NAME = 'robloxcam / rc', DESC = 'Allows you to freely move camera around the game (roblox version)'}
@@ -9,3 +10,4 @@ end)
 addcmd('robloxcam',{'rc'},function(args, speaker)
 	loadstring(game:HttpGet("https://pastebin.com/raw/GX6MytJm"))()
 end)
+]]
